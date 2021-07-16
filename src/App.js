c96667cb94ec0c67f07136ed56a7b1bc97420800
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-16 14:13:29 +0530

    import home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Home from './Components/home'
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   );
 }
