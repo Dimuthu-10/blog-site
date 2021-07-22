95fa1d6be285c4d41bff8453f6dc2c17f1c78bea
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-22 22:22:55 +0530

    updated with buton delete

--- src/Components/blog-item.jsx
+++ src/Components/blog-item.jsx
@@ -3,10 +3,11 @@ const BlogItems = ({ blog, title }) => {
     return ( 
         <div className="list-items">
             <h2> { title } </h2>
+
             {blog.map((blogs) => (
             <div className="blog-item" key={blogs.id}>
                 <ul className="list-group list-group-flush">
-                    <li className=" item list-group-item    m-1 ">
+                    <li className=" item list-group-item rounded   m-1 ">
                          <h2 className='header'>{ blogs.title }</h2>
                          <p>Written by { blogs.author } </p>
                     </li>
