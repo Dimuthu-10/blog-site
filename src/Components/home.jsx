c25cff6adc6d99dc17ecd1e43395c71ae7f030ab
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-22 22:23:29 +0530

    add ude effect function

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogItems from './blog-item';
 
 const Home = () => {
 
-    const [blog,setBlog] = useState([
-        {title: 'First Blog', body:'this is the blog body of the first blog', author: 'Dimuthu', id:1},
-        {title: 'Second Blog', body:'this is the blog body of the Second blog', author: 'Lakshan' ,id:2},
-        {title: 'Third Blog', body:'this is the blog body of the Third blog' ,author: 'Ashen', id:3}
-    ]);
+    const [blog,setBlog] = useState(null);
+
+   
+    
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlog(data)
+        })
+    }, [])
    
     return ( 
         <div className="container">
-           <BlogItems blog={ blog } title='All Blogs'/>
+           {blog && <BlogItems blog={ blog } title='All Blogs'  />}
         </div>
      );
 }
