7034b6068cc6943f7217c8c2c8a0180a41353897
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-23 22:08:05 +0530

    add router options

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Navbar from './Components/Navbar';
 import Home from './Components/home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
+      <Router>
+        <Navbar/>
+        <div>
+          <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
