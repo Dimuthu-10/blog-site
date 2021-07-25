ea7678f93435ef95ee6c50f66f9e22640275c3e1
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-25 12:25:13 +0530

    add usefetch hook

--- src/Components/BlogRead.jsx
+++ src/Components/BlogRead.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import UseFetch from "./useFetch";
 
 const BlogRead = () => {
 
     const { id } = useParams();
+    const { data: blog, error , pending} = UseFetch(`http://localhost:8000/blogs/${id}`);
 
     return ( 
         <div>
-            <h2>Blog Read - { id }</h2>
+            <h2>This is a body of { id } blog</h2>
+            { pending && <div>Loading...</div> }
+            { error && <div> { error } </div>}
+            { blog && (
+                <article>
+                    <h2> { blog.title } </h2>
+                    <div> { blog.body } </div>
+                    <p>Written by { blog.author } </p>
+                </article>
+            )}
         </div>
      );
 }
