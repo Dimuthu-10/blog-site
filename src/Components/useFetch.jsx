61a59778da436f8045203af94c658222df32d18e
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-25 12:26:23 +0530

    add abort controller

--- src/Components/useFetch.jsx
+++ src/Components/useFetch.jsx
@@ -1,27 +1,38 @@
 import  { useEffect, useState } from 'react'
 
+
+
 const useFetch = (url) => {
     const [data,setData] = useState(null);
     const [pending, setPending] = useState(true);
     const [error, setError] = useState(null);
     
     useEffect(() => {
-        fetch(url)
-        .then(res => {
-            if (!res.ok){
-                throw Error ('Could not fetch the data correctly');
-            }
-            return res.json();
-        })
-        .then(data => {
-            setData(data);
-            setPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setError(err.message);
-            setPending(false);
-        });
+
+        const abrt = new AbortController();
+
+        fetch(url,{ signal:abrt.signal })
+            .then(res => {
+                if (!res.ok){
+                    throw Error ('');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setPending(false);
+                setData(data);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError'){
+                    console.log('Fetch Aborted')
+                }
+                else{
+                    setPending(false);
+                    setError(err.message);
+                }
+            });
+            return () => abrt.abort();
     }, [url])
 
     return ( 
