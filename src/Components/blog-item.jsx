3857ae6a9791f847128eacec104764e58344e5ad
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-25 12:24:57 +0530

    add link

--- src/Components/blog-item.jsx
+++ src/Components/blog-item.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const BlogItems = ({ blog, title }) => {
-    // const blogs = props.blog
+    //  const blogs = props.blog
     return ( 
         <div className="list-items">
             <h2> { title } </h2>
@@ -10,7 +10,7 @@ const BlogItems = ({ blog, title }) => {
             <div className="blog-item" key={blogs.id}>
                 <ul className="list-group list-group-flush">
                     <li className="item list-group-item rounded   m-1 ">
-                        <Link to={`/blogs/ ${blogs.id}  `}>
+                        <Link to={`/blogs/ ${blogs.id} `}>
                             <h2 className='header'>{ blogs.title }</h2>
                             <p>Written by { blogs.author } </p>
                         </Link>
