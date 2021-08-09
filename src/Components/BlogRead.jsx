7d26715d6275786ef1e572a54b4a8046ad7b823f
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-08-09 12:49:33 +0530

    add delete fetch request

--- src/Components/BlogRead.jsx
+++ src/Components/BlogRead.jsx
@@ -1,10 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import UseFetch from "./useFetch";
 
 const BlogRead = () => {
 
     const { id } = useParams();
     const { data, error , pending} = UseFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory();
+
+    const clickHandle= () => {
+        fetch(`http://localhost:8000/blogs/${id}`,{
+            method: "DELETE",
+
+        })
+        .then(()=>{
+            console.log('blog deleted');
+            history.push('/');
+        })
+    }
 
     return ( 
         <div>
@@ -15,6 +27,7 @@ const BlogRead = () => {
                     <h2> { data.title } </h2>
                     <div> { data.body } </div>
                     <p>Written by { data.author } </p>
+                    <button onClick={clickHandle} >Delete Blog</button>
                 </article>
             )}
         </div>
