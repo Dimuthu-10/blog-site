df6151d12f7191538cd3fb75b75e618f9785fb3f
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-08-21 14:38:28 +0530

    add update path

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
             <Route path= '/blogs/:id'>
               <BlogRead/>
             </Route>
-            <Route path= '/Update'>
+            <Route path= '/Update/:id'>
               <Update/>
             </Route>
             <Route path='*'>
